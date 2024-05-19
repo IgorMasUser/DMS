22ee722cbdddedf1f7d713ebb29caa1ca13a4dec
Igor <igormasmas1@gmail.com>
2024-05-19 19:43:49 +0200

    Files downloading implemented

--- Dms.Api/Pages/DocumentsData/DocumentDetails.razor.cs
+++ Dms.Api/Pages/DocumentsData/DocumentDetails.razor.cs
@@ -2,6 +2,8 @@
 using Dms.Core.Application.Common.DTOs;
 using Dms.Core.Application.Common.Interfaces;
 using Microsoft.AspNetCore.Components;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.JSInterop;
 using MudBlazor;
 
 namespace Dms.Api.Pages.DocumentsData
@@ -9,12 +11,35 @@ namespace Dms.Api.Pages.DocumentsData
     public partial class DocumentDetails
     {
         [Inject] private IDmsDbContext dbContext { get; set; }
-
+        [Inject] public IJSRuntime JS { get; set; }
         [Inject] public IMapper Mapper { get; set; }
+        [Inject] public ILogger<DocumentDetails> logger { get; set; }
 
         async Task OnFormSave()
         {
-        
+
+        }
+
+        async Task DownloadFile(string documentNumber)
+        {
+            try
+            {
+                var file = await dbContext.FilesData.FirstOrDefaultAsync(dn => dn.DocumentNumber.Equals(documentNumber));
+
+                if (file == null) throw new FileNotFoundException($"No file found with DocumentNumber: {documentNumber}");
+
+                using (MemoryStream ms = new MemoryStream(file.Data))
+                {
+                    using var streamRef = new DotNetStreamReference(stream: ms);
+
+                    await JS.InvokeVoidAsync("downloadFileFromStream", file.FileName, streamRef);
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.LogError($"File not found: {ex.Message}");
+            }
+            
         }
     }
 }
